428a04d5a64e345d93b87720617a3a2284e96aa1
kofta999 <mostafaxxx555@gmail.com>
2025-01-07 13:46:50 +0200

    Complete step 4

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,8 @@ const Config = struct {
     output_file: ?[]const u8 = null,
     /// (-c, --count) adds a new first column that contains a count of the number of times a line appears in the input file
     count: bool = false,
+    /// (-d, --repeated) outputs only repeated lines
+    repeated: bool = false,
 
     fn streql(a: []const u8, b: []const u8) bool {
         return std.mem.eql(u8, a, b);
@@ -27,6 +29,8 @@ const Config = struct {
             } else if (!is_input_set) {
                 is_input_set = true;
                 config.input_file = arg;
+            } else if (streql(arg, "-d") or streql(arg, "--repeated")) {
+                config.repeated = true;
             } else {
                 config.output_file = arg;
             }
@@ -58,7 +62,7 @@ pub fn main() !void {
         file = try std.fs.cwd().readFileAlloc(allocator, config.input_file.?, 1024 * 1024);
     }
 
-    const res = try uniq(allocator, file, config.count);
+    const res = try uniq(allocator, file, config.count, config.repeated);
     defer allocator.free(res);
 
     if (config.output_file == null) {
@@ -72,14 +76,14 @@ pub fn main() !void {
     }
 }
 
-fn uniq(allocator: Allocator, file: []u8, count: bool) ![]u8 {
+fn uniq(allocator: Allocator, file: []u8, count: bool, repeated: bool) ![]u8 {
     var file_lines = std.mem.splitScalar(u8, file, '\n');
 
     var res = std.ArrayList(u8).init(allocator);
     // Won't do anything
     defer res.deinit();
 
-    var prev: []const u8 = "";
+    var prev: []const u8 = file_lines.next().?;
     var i: u32 = 1;
 
     while (file_lines.next()) |line| {
@@ -88,18 +92,31 @@ fn uniq(allocator: Allocator, file: []u8, count: bool) ![]u8 {
             continue;
         }
 
-        prev = line;
+        if (repeated) {
+            if (i > 1) {
+                if (count) {
+                    var buf: [256]u8 = undefined;
+                    const str = try std.fmt.bufPrint(&buf, "{}", .{i});
 
-        if (count) {
-            var buf: [256]u8 = undefined;
-            const str = try std.fmt.bufPrint(&buf, "{}", .{i});
+                    try res.appendSlice(str);
+                    try res.append(' ');
+                }
+                try res.appendSlice(prev);
+                try res.append('\n');
+            }
+        } else {
+            if (count) {
+                var buf: [256]u8 = undefined;
+                const str = try std.fmt.bufPrint(&buf, "{}", .{i});
 
-            try res.appendSlice(str);
-            try res.append(' ');
+                try res.appendSlice(str);
+                try res.append(' ');
+            }
+            try res.appendSlice(prev);
+            try res.append('\n');
         }
 
-        try res.appendSlice(line);
-        try res.append('\n');
+        prev = line;
         i = 1;
     }
 
