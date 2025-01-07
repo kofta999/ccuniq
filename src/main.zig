a276ddd77e094767a1c9ce3c72ede4c1ca9e1a81
kofta999 <mostafaxxx555@gmail.com>
2025-01-07 12:52:36 +0200

    Improve CLI parsing

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,40 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const ArgIterator = std.process.ArgIterator;
+
+const Config = struct {
+    input_file: ?[]const u8 = null,
+    output_file: ?[]const u8 = null,
+    /// (-c, --count) adds a new first column that contains a count of the number of times a line appears in the input file
+    count: bool = false,
+
+    fn streql(a: []const u8, b: []const u8) bool {
+        return std.mem.eql(u8, a, b);
+    }
+
+    fn init(args: *ArgIterator) Config {
+        var config = Config{};
+        var is_input_set = false;
+
+        // Skip exe name
+        _ = args.next();
+
+        while (args.next()) |arg| {
+            if (streql(arg, "-c") or streql(arg, "--count")) {
+                config.count = true;
+            } else if (streql(arg, "-")) {
+                is_input_set = true;
+            } else if (!is_input_set) {
+                is_input_set = true;
+                config.input_file = arg;
+            } else {
+                config.output_file = arg;
+            }
+        }
+
+        return config;
+    }
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -9,31 +44,27 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    const args = try std.process.argsAlloc(std.heap.page_allocator);
-    defer std.process.argsFree(std.heap.page_allocator, args);
+    var args = std.process.args();
+    const config = Config.init(&args);
 
-    if (args.len < 2) return error.ExpectedFileName;
-
-    const output_file: ?[]u8 = if (args.len == 3) args[2] else null;
-    const filename = args[1];
     var file: []u8 = undefined;
 
-    if (std.mem.eql(u8, filename, "-")) {
+    if (config.input_file == null) {
         const stdin = std.io.getStdIn().reader();
         file = try stdin.readAllAlloc(allocator, 1024 * 1024);
     } else {
         // TODO: Try reading in chunks
         // 1MB max size currently
-        file = try std.fs.cwd().readFileAlloc(allocator, filename, 1024 * 1024);
+        file = try std.fs.cwd().readFileAlloc(allocator, config.input_file.?, 1024 * 1024);
     }
 
     const res = try removeDuplicateAdjacent(allocator, file);
 
-    if (output_file == null) {
+    if (config.output_file == null) {
         try stdout.print("{s}", .{res});
         try bw.flush();
     } else {
-        const file_writer = try std.fs.cwd().createFile(output_file.?, .{});
+        const file_writer = try std.fs.cwd().createFile(config.output_file.?, .{});
         defer file_writer.close();
 
         try file_writer.writeAll(res);
